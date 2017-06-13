7b18ac11ab647b6295b024abcd26a3e3ed0a1c57
Carlos Millett <carlos4735@gmail.com>
2017-06-13 01:18:59 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 #/usr/bin/env python3
 
+#
 # A setuptools based setup module.
 #
-# See:
-# https://packaging.python.org/en/latest/distributing.html
-# https://github.com/pypa/sampleproject
 
 
-from setuptools import setup, find_packages
-from codecs import open
-from os import path
+from setuptools import setup
 
 import renamer
 
@@ -17,11 +13,14 @@ import renamer
 setup(
     name = "renamer",
     version = renamer.__version__,
+
     description = "A utility to rename TV files.",
     long_description = renamer.__doc__,
     url = "https://github.com/caedus75/Renamer",
+
     author = renamer.__author__,
     author_email = renamer.__email__,
+
     license = "BSD",
 
     packages = [
@@ -38,23 +37,18 @@ setup(
     keywords = "utility rename tv show",
 
     classifiers = [
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         "Development Status :: 4 - Beta",
 
-        # Indicate who your project is intended for
-        "Intended Audience :: End Users/Desktop"
+        "Intended Audience :: End Users/Desktop",
         "Topic :: Utilities",
 
-        # Pick your license as you wish (should match "license" above)
         "License :: OSI Approved :: BSD License",
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
     ],
+
+    python_requires = ">=3.4"
 )
