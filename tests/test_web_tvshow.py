12739c27d9b8c6c2ae2b2d2b6d558aba14b905e4
Carlos Millett <carlos4735@gmail.com>
2017-12-09 12:52:56 -0200

    create tests for web module

---
+++ tests/test_web_tvshow.py
@@ -0,0 +1,49 @@
+import pytest
+
+from renamer import web
+
+
+def test_tvshow_simple():
+    name = 'Wanted'
+    test = web.TvShow(name)
+    assert test.title == 'Wanted'
+    # assert test.thetvdb == '78809'
+
+
+
+def broken_test_tvshow_numbers():
+    name = '11 22 63'
+    test = web.TvShow(name)
+    assert test.title == '11.22.63'
+    # assert test.thetvdb == '301824'
+
+
+def test_tvshow_with_year():
+    name = 'The Goldbergs'
+    year = '2013'
+    test = web.TvShow(name, year=year)
+    assert test.title == 'The Goldbergs'
+    # assert test.thetvdb == '269653'
+
+
+def test_tvshow_with_country():
+    name = 'Wanted'
+    country = 'AU'
+    test = web.TvShow(name, country=country)
+    assert test.title == 'Wanted'
+    # assert test.thetvdb == '271360'
+
+
+def test_tvshow_full():
+    name = 'Wanted'
+    country = 'AU'
+    year = '2016'
+    test = web.TvShow(name, country=country, year=year)
+    assert test.title == 'Wanted'
+    # assert test.imdb == 'tt5193358'
+
+
+def test_tvshow_error():
+    name = 'Wrong Name'
+    with pytest.raises(web.NotFoundError):
+        web.TvShow(name)
