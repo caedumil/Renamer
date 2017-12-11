7d5e7f75cd5a107fe26fe616fa5551fd97017b1e
Carlos Millett <carlos4735@gmail.com>
2017-12-11 00:37:25 -0200

    update web tests

--- tests/test_web_tvshow.py
+++ tests/test_web_tvshow.py
@@ -7,15 +7,14 @@ def test_tvshow_simple():
     name = 'Wanted'
     test = web.TvShow(name)
     assert test.title == 'Wanted'
-    # assert test.thetvdb == '78809'
-
+    assert test.thetvdb == 78809
 
 
 def broken_test_tvshow_numbers():
     name = '11 22 63'
     test = web.TvShow(name)
     assert test.title == '11.22.63'
-    # assert test.thetvdb == '301824'
+    assert test.thetvdb == 301824
 
 
 def test_tvshow_with_year():
@@ -23,7 +22,7 @@ def test_tvshow_with_year():
     year = '2013'
     test = web.TvShow(name, year=year)
     assert test.title == 'The Goldbergs'
-    # assert test.thetvdb == '269653'
+    assert test.thetvdb == 269653
 
 
 def test_tvshow_with_country():
@@ -31,7 +30,7 @@ def test_tvshow_with_country():
     country = 'AU'
     test = web.TvShow(name, country=country)
     assert test.title == 'Wanted'
-    # assert test.thetvdb == '271360'
+    assert test.thetvdb == 271360
 
 
 def test_tvshow_full():
@@ -40,7 +39,7 @@ def test_tvshow_full():
     year = '2016'
     test = web.TvShow(name, country=country, year=year)
     assert test.title == 'Wanted'
-    # assert test.imdb == 'tt5193358'
+    assert test.thetvdb == 304591
 
 
 def test_tvshow_error():
