4abaedff6172612c1f34523eba155b3c707d4fec
Carlos Millett <carlos4735@gmail.com>
2018-10-07 02:09:19 -0300

    remove old python version from setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         "License :: OSI Approved :: BSD License",
 
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
     ],
