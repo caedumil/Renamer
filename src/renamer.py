79a77c45130c5c047899d1b351935f69fca23455
Carlos <carlos4735@gmail.com>
2018-12-14 13:44:21 -0300

    add script to be used with pyinstaller

---
+++ src/renamer.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2014 - 2018, Carlos Millett
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the Simplified BSD License.  See the LICENSE file for details.
+
+import sys
+
+from renamer import cli
+from renamer import log
+
+
+def main():
+    parser = cli.setParser()
+    args = parser.parse_args()
+    logger = log.setupLogger()
+
+    return cli.main(args, logger)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
