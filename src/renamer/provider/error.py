bcd22a712992f2003c244244c59d4e048be08765
Carlos Millett <carlos4735@gmail.com>
2019-04-19 16:08:00 -0300

    add provider module

---
+++ src/renamer/provider/error.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2014 - 2018, Carlos Millett
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the Simplified BSD License.  See the LICENSE file for details.
+
+
+class DownloadError(Exception):
+    '''Raise when page cannot be downloaded.'''
+
+
+class NotFoundError(Exception):
+    '''Raise when show can't be found in the remote database.'''
