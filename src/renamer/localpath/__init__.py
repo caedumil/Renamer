36327af80b944c108c2e9c061a51b57d9e0bab39
Carlos Millett <carlos4735@gmail.com>
2019-05-12 00:48:05 -0300

    cleanup localpath __init__

--- src/renamer/localpath/__init__.py
+++ src/renamer/localpath/__init__.py
@@ -3,7 +3,3 @@
 #
 # This software may be modified and distributed under the terms
 # of the Simplified BSD License.  See the LICENSE file for details.
-
-
-from .files import genFilesList
-from .shows import genShowList
