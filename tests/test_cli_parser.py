1e9ccd9053cbfbec569f7cf4326c025e97e6ff6c
Carlos Millett <carlos4735@gmail.com>
2021-03-22 14:13:02 -0300

    groups cli test in a test class

--- tests/test_cli_parser.py
+++ tests/test_cli_parser.py
@@ -4,19 +4,18 @@ from pathlib import Path
 from renamer import cli
 
 
-def test_cli_parser_noargs():
-    parser = cli.setParser()
-    with pytest.raises(SystemExit):
-        parser.parse_args([])
+class TestCLI:
+    def test_cli_parser_noargs(self):
+        parser = cli.setParser()
+        with pytest.raises(SystemExit):
+            parser.parse_args([])
 
+    def test_cli_parser_wrong_log(self):
+        parser = cli.setParser()
+        with pytest.raises(SystemExit):
+            parser.parse_args(['-l', 'wrong', 'test'])
 
-def test_cli_parser_wrong_log():
-    parser = cli.setParser()
-    with pytest.raises(SystemExit):
-        parser.parse_args(['-l', 'wrong', 'test'])
-
-
-def test_cli_parser_positional():
-    parser = cli.setParser()
-    args = parser.parse_args(['test', 'file', 'path'])
-    assert args.path == [Path('test'), Path('file'), Path('path')]
+    def test_cli_parser_positional(self):
+        parser = cli.setParser()
+        args = parser.parse_args(['test', 'file', 'path'])
+        assert args.path == [Path('test'), Path('file'), Path('path')]
