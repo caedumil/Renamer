9fce227b279dcf50fc112b62114d64bdf76bbfd1
Carlos Millett <carlos4735@gmail.com>
2021-04-11 16:28:22 -0300

    move setup.py configuration to setup.cfg
    and improve pyproject.toml

--- setup.py
+++ setup.py
@@ -1,58 +1,8 @@
-#
-# A setuptools based setup module.
-#
+#!/usr/bin/env python3
 
 
-import os
-
-from setuptools import setup, find_packages
+import setuptools
 
 
-def _read(fn):
-    path = os.path.join(os.path.dirname(__file__), fn)
-    return open(path).read()
-
-
-setup(
-    name="renamer",
-    version="0.3.1",
-
-    description="A utility to rename TV files.",
-    long_description=_read("README.md"),
-    url="https://github.com/caedus75/Renamer",
-
-    author="Carlos Millett",
-    author_email="carlos4735@gmail.com",
-
-    license="BSD",
-
-    packages=find_packages("src"),
-
-    package_dir={"": "src"},
-
-    entry_points={
-        "console_scripts": [
-            "renamer = renamer.__main__:main",
-        ],
-    },
-
-    install_requires=[],
-
-    keywords="utility rename tv show",
-
-    classifiers=[
-        "Development Status :: 4 - Beta",
-
-        "Intended Audience :: End Users/Desktop",
-        "Topic :: Utilities",
-
-        "License :: OSI Approved :: BSD License",
-
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9"
-    ],
-
-    python_requires=">=3.7"
-)
+if __name__ == '__main__':
+    setuptools.setup()
