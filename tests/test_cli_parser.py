e0a14931b1ddf8aa74261ec0df10eb2b384b8765
Carlos Millett <carlos4735@gmail.com>
2021-04-12 00:01:07 -0300

    improve cli tests

--- tests/test_cli_parser.py
+++ tests/test_cli_parser.py
@@ -14,6 +14,10 @@ class TestCLI:
         with pytest.raises(SystemExit):
             parser.parse_args([])
 
+    def test_cli_optional_only(self, parser):
+        with pytest.raises(SystemExit):
+            parser.parse_args(['--loglevel', 'WARN', '--season', '0'])
+
     def test_cli_parser_set_log_lowercase(self, parser):
         args = parser.parse_args(['-l', 'debug', 'test'])
         assert args.loglevel == 'DEBUG'
@@ -26,13 +30,19 @@ class TestCLI:
         with pytest.raises(SystemExit):
             parser.parse_args(['-l', 'wrong', 'test'])
 
-    def test_cli_parser_with_season(self, parser):
+    def test_cli_parser_season_overwrite(self, parser):
         args = parser.parse_args(['--season', '99', 'test'])
         assert args.season == 99
 
-    def test_cli_parser_without_season(self, parser):
+    def test_cli_parser_no_confirm_true(self, parser):
+        args = parser.parse_args(['--no-confirm', 'test'])
+        assert args.askuser is True
+
+    def test_cli_parser_defaults(self, parser):
         args = parser.parse_args(['test'])
+        assert args.loglevel == 'INFO'
         assert args.season == -1
+        assert args.askuser is False
 
     def test_cli_parser_positional(self, parser):
         args = parser.parse_args(['test', 'file', 'path'])
